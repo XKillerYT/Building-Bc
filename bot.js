b78c42ba2675dc760e28488d748a1155b8244fd1
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-26 19:40:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs')
 const client = new Discord.Client();
-البرفكس هنا
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
